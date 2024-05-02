d49ab89b86811ccea6d648c35287e96b0f043c0d
mohamed abdelmalek dorbani <m.abdelmalek.dorbani@gmail.com>
2024-05-03 00:41:11 +0200

    Css deployment fixing

--- src/champion.jsx
+++ src/champion.jsx
@@ -33,33 +33,54 @@ const ChampionPage = () => {
   return (
     <div className="champion-page">
       <div className='champion-container'>
-        <div className='champions-combat' style={{color:"black"}}>
-          {/* <img src={video2} alt="" /> */}
-          <div style={{display:"flex", justifyContent:"space-around", alignItems:"center"}}>
+        <div
+          className='champions-combat'
+          style={{
+            color: "black",
+            position: 'relative', // Position relative pour positionner absolument l'arrière-plan flou
+            overflow: 'hidden'   // Assurez-vous que tout contenu débordant est masqué
+          }}
+        >
+          {/* Élément de fond flou */}
+          <div style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            backgroundImage: `url(${champion.url})`,
+            backgroundSize: 'cover',
+            filter: 'blur(8px)',
+            // zIndex: -1 // Place cet élément derrière le contenu
+          }}></div>
+
+          {/* Contenu net */}
+          <div style={{ display: "flex", justifyContent: "space-around", alignItems: "center", height: '100%' }}>
             <div>
-            <h1 className='champion-name' style={{ marginTop:"20px",  paddingTop: "130px", paddingLeft: "50px" }}>
+              <h1 className='champion-name' style={{ marginTop: "20px", paddingTop: "130px", paddingLeft: "50px" }}>
                 {champion.name}
-            </h1>
-            <span className='vs' style={{ marginLeft: "10rem", fontSize:"25px" }}>VS</span>
-            <h1 className='champion-name' style={{ marginLeft: "10rem" }}>
-              {champion.Adversaire}
-            </h1>
-          </div>
+              </h1>
+              <span className='vs' style={{ marginLeft: "10rem", fontSize: "25px" }}>VS</span>
+              <h1 className='champion-name' >
+                {champion.Adversaire}
+              </h1>
+            </div>
             <div className='combatYear'>{champion.year}</div>
           </div>
         </div>
+
         <h1 className='QUIEST' >
           QUI-EST CE CHAMPION ?
         </h1>
-        <img className='image1' src={video2} alt="" />
+        <img className='image1' src={champion.url} alt="" />
         <div className='texte1'>
           {champion.QUIEST}
         </div>
-        <h1 className="QUIEST" style={{width:"72rem"}}>
+        <h1 className="QUIEST Face" style={{width:"72rem"}}>
           la Face caché <br /> de la victoire
         </h1>
         <div className='video'>
-          <img src={video2} alt="" />
+          <img src={champion.url} alt="" />
         </div>
         <div className='texteLaFace'>
           {champion.LaFaceCache}
@@ -68,7 +89,7 @@ const ChampionPage = () => {
           extrait video
         </h1>
         <div className='video2'>
-          <img src={video2} alt="" />
+          <img src={champion.url} alt="" />
         </div>
       </div>
     </div>
